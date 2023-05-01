1382d6ef3d5fd2c94be499f3afc59957f5f146ae
Mickael Martins <mickael.martins@aviation-civile.gouv.fr>
2023-05-02 00:03:13 +0200

    fix tcp disconnection detection

--- src/mirror/tcp_client.py
+++ src/mirror/tcp_client.py
@@ -20,9 +20,9 @@ class ClientThread(threading.Thread):
         address, _ , port = self.address.partition(":")
         while True:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
             try:
                 self.socket.connect((address,int(port)))
+                self.socket.settimeout(1)
                 self.logger.info(f"connection established with {address}:{port}")
             except:
                 self.logger.debug(f"connection refused from {address}:{port}")
@@ -30,18 +30,26 @@ class ClientThread(threading.Thread):
                 time.sleep(1)
             else:
                 while True:
+                    sent = None
                     item = self.queue.get()
                     try:
                         sent = self.socket.send(item)
                         self.logger.debug(f"data sent to {address}:{port} -> result: {sent}")
+                        data = self.socket.recv(1024)
+                        if not data:
+                            self.logger.info("no data")
+                            raise BrokenPipeError()                 
                     except BlockingIOError:
                         continue
                     except BrokenPipeError:
                         self.logger.error(f"disconnection from {address}:{port}")
                         break
-                    except TimeoutError:
-                        self.logger.debug(f"Timeout attempting to connect to {address}:{port}")
-                        break
+                    except TimeoutError as timeout:
+                        if sent == None:
+                            self.logger.debug(f"timeout sending {address}:{port}")
+                            break
+                        self.logger.debug(f"tcp connection alive {address}:{port}")                            
+                        
             finally:
                 self.logger.debug(f"closing connection{address}:{port}")
                 self.socket.close()
